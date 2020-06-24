3e74e9afd0240ae4a2ef9a2d5cc288da9ce72def
Isaque Azevedo <isaque.azevedo@sap.com>
2020-06-23 21:27:44 -0300

    feat: implement game logic

--- sketch.js
+++ sketch.js
@@ -1,25 +1,49 @@
 let imageScenario;
 let imageCharacter;
+let imageEnemy;
+
 let scenario;
 let gameMusic;
-let character
+let jumpSound;
+let character;
+let enemy;
 
 function preload() {
   imageScenario = loadImage('imagens/cenario/floresta.png');
   imageCharacter = loadImage('imagens/personagem/correndo.png');
+  imageEnemy = loadImage('imagens/inimigos/gotinha.png');
   gameMusic = loadSound('sons/trilha_jogo.mp3');
-  frameRate(60);
+  jumpSound = loadSound('sons/somPulo.mp3');
 }
-  
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   scenario = new Scenario(imageScenario, 2);
-  character = new Character(imageCharacter);
+  character = new Character(imageCharacter, 0, 220, 270, 220, 270, 4, 4);
+  enemy = new Enemy(imageEnemy, width - 50, 52, 52, 105, 104, 7, 4);
+  frameRate(30);
   gameMusic.loop();
+}
+
+function keyPressed() {
+  if (key === 'ArrowUp') {
+    character.jump();
+    jumpSound.play();
+  }
 }
 
 function draw() {
   scenario.draw();
   scenario.move();
+
   character.draw();
+  character.gravity();
+
+  enemy.draw();
+  enemy.move();
+
+  if (character.isColliding(enemy)) {
+    console.log("hit!");
+    noLoop();
+  }
 }
