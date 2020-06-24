7eec5c89f977ce90dcffbefe128fdde7d22897cb
Isaque Azevedo <isaque.azevedo@sap.com>
2020-06-24 01:52:14 -0300

    feat: new char and background options

--- sketch.js
+++ sketch.js
@@ -2,12 +2,24 @@ let imageScenario;
 let imageCharacter;
 let imageEnemy;
 let imageGameOver;
+let imageRunGif;
 
 let scenario;
 let gameMusic;
 let jumpSound;
 let character;
 let enemy;
+
+let imageScenario1;
+let imageScenario2;
+let imageScenario3;
+let imageScenario4;
+let imageScenario5;
+let scenario1;
+let scenario2;
+let scenario3;
+let scenario4;
+let scenario5;
 
 function preload() {
   imageScenario = loadImage('imagens/cenario/floresta.png');
@@ -16,6 +28,13 @@ function preload() {
   imageGameOver = loadImage('imagens/assets/game-over.png')
   gameMusic = loadSound('sons/trilha_jogo.mp3');
   jumpSound = loadSound('sons/somPulo.mp3');
+  imageRunGif = createImg("imagens/personagem/sprites/run outline.gif");
+
+  imageScenario1 = loadImage('imagens/cenario/jungle/plx-1.png');
+  imageScenario2 = loadImage('imagens/cenario/jungle/plx-2.png');
+  imageScenario3 = loadImage('imagens/cenario/jungle/plx-3.png');
+  imageScenario4 = loadImage('imagens/cenario/jungle/plx-4.png');
+  imageScenario5 = loadImage('imagens/cenario/jungle/plx-5.png');
 }
 
 function setup() {
@@ -25,6 +44,12 @@ function setup() {
   enemy = new Enemy(imageEnemy, width - 50, 52, 52, 105, 104, 7, 4);
   frameRate(30);
   gameMusic.loop();
+
+  scenario1 = new Scenario(imageScenario1, 0.2);
+  scenario2 = new Scenario(imageScenario2, .3);
+  scenario3 = new Scenario(imageScenario3, .4);
+  scenario4 = new Scenario(imageScenario4, .5);
+  scenario5 = new Scenario(imageScenario5, 1);
 }
 
 function keyPressed() {
@@ -38,14 +63,32 @@ function keyPressed() {
 }
 
 function draw() {
-  scenario.draw();
-  scenario.move();
+  // OPTION 1
+  // scenario.draw();
+  // scenario.move();
 
-  character.draw();
-  character.gravity();
+  // character.draw();
+  // character.gravity();
 
   enemy.draw();
   enemy.move();
+
+  // OPTION 2
+
+  scenario1.draw();
+  scenario2.draw();
+  scenario3.draw();
+  scenario4.draw();
+  scenario5.draw();
+  
+  scenario1.move();
+  scenario2.move();
+  scenario3.move();
+  scenario4.move();
+  scenario5.move();
+
+  imageRunGif.size(46,68)
+  imageRunGif.position(50, height - 100);
 
   if (character.isColliding(enemy)) {
     image(imageGameOver, 100, 100, width - 200, height - 300);
