c20c8fdced656b2a4a16ba2cbc30b5c58f7dae3d
Isaque Azevedo <isaque.azevedo@sap.com>
2020-06-25 16:57:00 -0300

    feat: add new enemies

--- scripts/game/enemy.js
+++ scripts/game/enemy.js
@@ -1,14 +1,16 @@
 class Enemy extends Animation {
-	constructor(image, x, width, height, spriteWidth, spriteHeight, rows, columns) {
-		super(image, x, width, height, spriteWidth, spriteHeight, rows, columns);
+	constructor(image, x, yDiff, width, height, spriteWidth, spriteHeight, rows, columns, velocity, delay) {
+		super(image, x, yDiff, width, height, spriteWidth, spriteHeight, rows, columns);
 
-		this.velocity = 10;
+		this.velocity = velocity;
+		this.delay = delay;
+		this.x = width + this.delay;
 	}
 
 	move() {
 		this.x = this.x - this.velocity;
 
-		if (this.x < -this.width) {
+		if (this.x < -this.width - this.delay) {
 			this.x = width;
 		}
 	}
