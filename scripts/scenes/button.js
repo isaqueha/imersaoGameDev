27d27a6a04108826b9e8ae10a5e214ae43025dd5
Isaque Azevedo <isaque.azevedo@sap.com>
2020-06-26 17:25:15 -0300

    feat: adding button

---
+++ scripts/scenes/button.js
@@ -0,0 +1,20 @@
+class Button {
+	constructor(text, x, y) {
+		this.text = text;
+		this.x = x;
+		this.y = y;
+		this.button = createButton(this.text);
+		this.button.mousePressed(() => this._changeScene());
+		this.button.addClass('botao-tela-inicial');
+	}
+
+	draw() {
+		this.button.position(this.x, this.y);
+		this.button.center('horizontal');
+	}
+
+	_changeScene() {
+		this.button.remove();
+		currentScene = 'game';
+	}
+}
